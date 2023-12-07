889e165093dea5b7c088cda668950fa3b510d9bb
carlsonh <carlsonh@student.21-school.ru>
2023-12-07 19:02:35 +0700

    added implementation abs/fabs/ceil/floor

---
+++ src/s21_math.h
@@ -0,0 +1,13 @@
+// s21_math.h
+#ifndef S21_MATH_H
+#define S21_MATH_H
+
+#define SUCCESS 0
+#define ERROR 1
+
+int s21_abs(int x);
+long double s21_ceil(double x);
+long double s21_floor(double x);
+long double s21_fabs(double x);
+
+#endif  // S21_MATH_H
