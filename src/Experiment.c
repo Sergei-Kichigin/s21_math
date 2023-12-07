9170972e3630270f7e87bcce65f2bb0de85b7fde
Georgiy-JO <wkone.mail@gmail.com>
2023-12-07 20:18:49 +0700

    Last Working (almost) Date: 12.7.23: 1)exp is mearged in, nut need to be fixed 2)pow done, need to be checked after exp fix 3)Makefile adjusted
    all in all done: log, pow, abs, fabs, ceil, floor, exp(need fix for negative numbers), test (beta), Makefile, s21_math.h

--- src/Experiment.c
+++ src/Experiment.c
@@ -4,37 +4,47 @@
 
 // int main() { printf("%.100lf", pow(exp(100), (double)1 / 100)); }
 
-#define MOD (long long)10e18
+// #define MOD (long long)10e18
 
-long double bin_pow(long double base, long double p);
-long double pnorm(long double in);
+// long double bin_pow(long double base, long double p);
+// long double pnorm(long double in);
+
+// int main() {
+//   long double in, times, res;
+
+//   while (in != -5) {
+//     scanf("%Lf%Lf", &in, &times);
+//     res = bin_pow(in, times);
+//     printf("%.20Lf\n", res);
+//   }
+// }
+
+// long double bin_pow(long double base, long double p) {
+//   if (p == 0) {
+//     return 1;  // Выход из рекурсии.
+//   }
+//   if (p > 0) {
+//     if (p == (long long)p && (long long)p % 2 == 0) {
+//       long double t = bin_pow(base, p / 2);
+//       return pnorm(t * t);
+//     } else {
+//       return pnorm(bin_pow(base, p - 1) * base);
+//     }
+//   } else {
+//     return pnorm(bin_pow(base, p + 1) / base);
+//   }
+// }
+// long double pnorm(long double in) {
+//   long long tmp = (long long)in;
+//   return (in >= 0) ? tmp % MOD + in - tmp : ((-tmp) % MOD - in + tmp) * (-1);
+// }
 
 int main() {
   long double in, times, res;
 
-  while (in != -5) {
+  while (in != -12345) {
     scanf("%Lf%Lf", &in, &times);
-    res = bin_pow(in, times);
-    printf("%.20Lf\n", res);
+    res = pow(in, times);
+    printf("%.16Lf\n", res);
   }
-}
-
-long double bin_pow(long double base, long double p) {
-  if (p == 0) {
-    return 1;  // Выход из рекурсии.
-  }
-  if (p > 0) {
-    if (p == (long long)p && (long long)p % 2 == 0) {
-      long double t = bin_pow(base, p / 2);
-      return pnorm(t * t);
-    } else {
-      return pnorm(bin_pow(base, p - 1) * base);
-    }
-  } else {
-    return pnorm(bin_pow(base, p + 1) / base);
-  }
-}
-long double pnorm(long double in) {
-  long long tmp = (long long)in;
-  return (in >= 0) ? tmp % MOD + in - tmp : ((-tmp) % MOD - in + tmp) * (-1);
 }
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,20 +1,5 @@
 #include "ln/s21_ln.h"
-// #include "pow/s21_pow.h"
 // #include "cos/s21_cos.h"
-#include "pow/s21_pow.h"
-#include "exp/s21_exp.h"
 #include "abs_fabs_ceil_floor/s21_abs_fabs_ceil_floor.h"
-
-// long double cos(double x);
-int s21_abs(int x);
-long double s21_ceil(double x);
-long double s21_floor(double x);
-long double s21_fabs(double x);
-
-
-long double ln_my(double x);
-
-
-long double pow(long double base, long double p);
-
-
+#include "exp/s21_exp.h"
+#include "pow/s21_pow.h"
