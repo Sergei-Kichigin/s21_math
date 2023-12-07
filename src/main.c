c63c49e768999e142eab715aeef0fc9c8ec2992d
lera <lera@Ubuntu.myguest.virtualbox.org>
2023-12-07 19:45:07 +0700

    s21_exp

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <math.h>  // ---------UDALiiiiiiiT'
+
+#include "s21_exp.h"
+
+int main() {
+  double x;
+  scanf("%lf", &x);
+  printf("%Lf", s21_exp(x));
+  printf("\n%lf", exp(x));  // ---------UDALiiiiiiiT'
+  return 0;
+}
