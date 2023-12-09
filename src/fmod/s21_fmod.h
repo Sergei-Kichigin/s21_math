ebfba31d4467a6e8b59be98b9d9179a926deb988
t1m0f3y <t.leonenko2001@gmail.com>
2023-12-09 16:57:35 +0700

    +fmod
    also add fmod to tests test_riderkri

---
+++ src/fmod/s21_fmod.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+long double s21_fmod(double x, double y);
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,5 +1,6 @@
 #include "abs_fabs_ceil_floor/s21_abs_fabs_ceil_floor.h"
 #include "exp/s21_exp.h"
+#include "fmod/s21_fmod.h"
 #include "ln/s21_ln.h"
 #include "pow_sqrt/s21_pow_sqrt.h"
 #include "trigonometry/s21_cos_sin_tan.h"
