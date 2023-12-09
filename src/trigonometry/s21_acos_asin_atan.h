744e1ca5ed8dd01ac7ff48cecf255fc6d958d569
archoldf <archoldf@student.21-school.ru>
2023-12-09 18:33:32 +0700

    add: s21_acos_asin_atan.h s21_acos_asin_atan.c

---
+++ src/trigonometry/s21_acos_asin_atan.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "../abs_fabs_ceil_floor/s21_abs_fabs_ceil_floor.h"
+#include "../pow_sqrt/s21_pow_sqrt.h"
+
+#define EPSilon 1E-18
+#define PI 3.14159265358979323846
+#define PI_2 2.0 * PI
+#define ONE 1.0
+#define MINUS (-1.0)
+
+long double s21_acos(double x);
+long double s21_asin(double x);
