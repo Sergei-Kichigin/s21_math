051cad468442e84313ea37e6f9ebea35aa8dd6ec
carlsonh <seroho@Seroho-PC.>
2023-12-16 17:23:02 +0700

    fix floor

--- src/abs_fabs_ceil_floor/s21_abs_fabs_ceil_floor.c
+++ src/abs_fabs_ceil_floor/s21_abs_fabs_ceil_floor.c
@@ -1,9 +1,15 @@
 #include "s21_abs_fabs_ceil_floor.h"
 
 int s21_abs(int x) { return (x < 0) ? -x : x; }
+
 long double s21_ceil(double x) {
   long longX = (long)x;
   return (x == (double)longX) ? longX : (x > 0) ? longX + 1 : longX;
 }
-long double s21_floor(double x) { return -(s21_ceil(-x)); }
+
+long double s21_floor(double x) {
+  long longX = (long)x;
+  return (x == (double)longX) ? longX : (x > 0) ? longX : longX - 1;
+}
+
 long double s21_fabs(double x) { return (x < 0) ? -x : x; }
