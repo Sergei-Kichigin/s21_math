d60ad9dae5b4bd324ccc2918486aee7099849028
Arch Oldfather <archoldf@cl-b4.nsk.21-school.ru>
2023-12-16 20:48:06 +0700

    add: inf -inf

--- src/abs_fabs_ceil_floor/s21_abs_fabs_ceil_floor.c
+++ src/abs_fabs_ceil_floor/s21_abs_fabs_ceil_floor.c
@@ -3,13 +3,39 @@
 int s21_abs(int x) { return (x < 0) ? -x : x; }
 
 long double s21_ceil(double x) {
-  long longX = (long)x;
-  return (x == (double)longX) ? longX : (x > 0) ? longX + 1 : longX;
+  long double longX = (long)x;
+  long double answer;
+
+  if (x == (double)longX) {
+    answer = longX;
+  } else if (x >= 0) {
+    answer = longX + 1;
+  } else {
+    if (x < 0.0 && x > -1.0) {  // краевой случай для вывода -0.000000
+      answer = -longX;
+    } else {
+      answer = longX;
+    }
+  }
+
+  return answer;
 }
 
 long double s21_floor(double x) {
-  long longX = (long)x;
-  return (x == (double)longX) ? longX : (x > 0) ? longX : longX - 1;
+  if (x == InFP || x == InFN) return x;
+
+  long double longX = (long)x;
+  long double answer = 0;
+
+  if (x == (double)longX) {
+    answer = longX;
+  } else if (x >= 0) {
+    answer = longX;
+  } else {
+    answer = longX - 1;
+  }
+
+  return answer;
 }
 
 long double s21_fabs(double x) { return (x < 0) ? -x : x; }
