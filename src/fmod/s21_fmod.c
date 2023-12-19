6ddfe37f0861cde02691ef36a4a165402a738caf
t1m0f3y <t.leonenko2001@gmail.com>
2023-12-19 19:02:01 +0700

    remove useless magick

--- src/fmod/s21_fmod.c
+++ src/fmod/s21_fmod.c
@@ -6,13 +6,8 @@ long double s21_fmod(double x, double y) {
   if (s21_fabs(y) == InFP) {
     res = x;
   } else {
-    int sign_x = 1;
-    if (x < 0) sign_x = -1;
     long double temp = s21_truncate(x / y);
     res = (long double)x - temp * (long double)y;
-    if (s21_fabs(res) == Fmod_magic_number) {
-      res = sign_x * s21_fabs(y) + res;
-    }
   }
   return res;
 }
@@ -20,5 +15,3 @@ long double s21_fmod(double x, double y) {
 long double s21_truncate(double x) {
   return x < 0 ? -s21_floor(-x) : s21_floor(x);
 }
-
-long double s21_fma(double x, double y, double z) { return (x * y) + z; }
